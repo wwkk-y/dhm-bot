202ac9618e23a7a7841867fa7b1d56c8ee1e29cf
韦永志 <3262954152@qq.com>
2024-06-03 22:28:28 +0800

    添加测试用例

--- src/plugins/dhm-plugin/app.js
+++ src/plugins/dhm-plugin/app.js
@@ -8,6 +8,6 @@ MsgSubUtil.subContentContainR(['你好'], (msg) => {
     return "你好, 我是dhm-bot, 很高兴认识你";
 }, {atMe: true})
 
-MsgSubUtil.subContentR(['你好'], (msg) => {
+MsgSubUtil.subContentR('你好', (msg) => {
     return "你好呀"
 }, {message_type: "private"})
